f20146c0f7be9ee5995b57e2dbe92fda55e83281
pedrohgr <pedrohgr2005@gmail.com>
2026-09-21 19:31:26 -0300

    refactor(lexer): expor componentes da issue 8

--- src/lexer/__init__.py
+++ src/lexer/__init__.py
@@ -1,7 +1,16 @@
 """Módulo do analisador léxico do compilador PyToJava."""
 
 from .char_stream import CharStream
+from .errors import LexicalError
+from .symbol_table import Symbol, SymbolTable
 from .token import Token
 from .token_type import TokenType
 
-__all__ = ["CharStream", "Token", "TokenType"]
+__all__ = [
+    "CharStream",
+    "LexicalError",
+    "Symbol",
+    "SymbolTable",
+    "Token",
+    "TokenType",
+]
