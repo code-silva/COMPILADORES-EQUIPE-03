98d912d54e2cbccae847534c3657229c7c114041
pedrohgr <pedrohgr2005@gmail.com>
2026-09-21 22:04:15 -0300

    refactor(lexer): exportar funcoes da tabela de simbolos (#8)

--- src/lexer/__init__.py
+++ src/lexer/__init__.py
@@ -2,7 +2,14 @@
 
 from .char_stream import CharStream
 from .errors import LexicalError
-from .symbol_table import Symbol, SymbolTable
+from .symbol_table import (
+    Symbol,
+    SymbolTable,
+    contains_symbol,
+    create_symbol_table,
+    insert_symbol,
+    lookup_symbol,
+)
 from .token import Token
 from .token_type import TokenType
 
@@ -11,6 +18,10 @@ __all__ = [
     "LexicalError",
     "Symbol",
     "SymbolTable",
+    "contains_symbol",
+    "create_symbol_table",
+    "insert_symbol",
+    "lookup_symbol",
     "Token",
     "TokenType",
 ]
