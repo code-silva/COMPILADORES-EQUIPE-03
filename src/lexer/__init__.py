93e813d42f21bc3f2dc9e6cd97520dbee7121eb0
SUDOTMOX <242024923@aluno.unb.br>
2026-09-21 22:36:23 -0300

    Revert "feat(lexer): implementar tabela de símbolos e erros léxicos (#8)"

--- src/lexer/__init__.py
+++ src/lexer/__init__.py
@@ -1,27 +1,7 @@
 """Módulo do analisador léxico do compilador PyToJava."""
 
 from .char_stream import CharStream
-from .errors import LexicalError
-from .symbol_table import (
-    Symbol,
-    SymbolTable,
-    contains_symbol,
-    create_symbol_table,
-    insert_symbol,
-    lookup_symbol,
-)
 from .token import Token
 from .token_type import TokenType
 
-__all__ = [
-    "CharStream",
-    "LexicalError",
-    "Symbol",
-    "SymbolTable",
-    "contains_symbol",
-    "create_symbol_table",
-    "insert_symbol",
-    "lookup_symbol",
-    "Token",
-    "TokenType",
-]
+__all__ = ["CharStream", "Token", "TokenType"]
