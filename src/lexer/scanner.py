ded50f88811198e60bc0b170d0b1f84c7a689040
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-09-24 17:00:13 -0300

    feat: :sparkles: integra IndentManager ao Scanner para identificação da indentação

--- src/lexer/scanner.py
+++ src/lexer/scanner.py
@@ -1,6 +1,8 @@
 from src.lexer.char_stream import CharStream
 from src.lexer.token import Token
 from src.lexer.token_type import TokenType
+from collections import deque
+from src.lexer.indent_manager import (finalize_indentation, process_indentation, reset_indent_manager)
 
 class Scanner:
 
@@ -22,6 +24,9 @@ class Scanner:
     #construtor com a instância do CharStream
     def __init__(self, source_code: str):
         self.stream = CharStream(source_code)
+        self.pending_tokens = deque()
+        self.at_line_start = True
+        reset_indent_manager()
 
     #verifica se é int ou float 
     def _scan_number(self, line: int, column: int) -> Token:    
@@ -160,23 +165,77 @@ class Scanner:
             self.stream.advance()
             return Token(TokenType.OP_GT, ">", None, line, column)
        
-    #aqui ignora linha de comentário e espaços em branco
-    def _skip_whitespace_and_comments(self) -> None:
-        while not self.stream.is_at_end():
-            char = self.stream.peek()
-            if char in (' ', '\t', '\n', '\r'):
-                self.stream.advance()
-            elif char == '#':
-                while not self.stream.is_at_end() and self.stream.peek() != '\n':
-                    self.stream.advance()
-            else:        
-                break        
+    #aqui ignora espaços em branco
+    def _skip_whitespace(self) -> None:
+        while not self.stream.is_at_end() and self.stream.peek() in (" ", "\t"):
+            self.stream.advance()
 
+    #aqui ignora comentário
+    def _skip_comment(self) -> None:
+        while not self.stream.is_at_end() and self.stream.peek() not in ("\n", "\r",):
+            self.stream.advance()
+
+    #aqui scanea nova linha e indica que vai inicializar uma nova 
+    def _scan_newline(self) -> None:
+        char = self.stream.advance()
+        if char == "\r" and self.stream.peek() == "\n":
+            self.stream.advance()
+        self.at_line_start = True    
+
+    #esse metodo processa a indentação
+    def _process_line_indentation(self) -> None:
+        self.at_line_start = False
+        start_line = self.stream.line
+        start_col = self.stream.column
+
+        #aqui é onde o recuo é contado
+        indent_level = 0
+        while self.stream.peek() in (" ", "\t"):
+            if self.stream.peek() == "\t":
+                indent_level += 4
+            else:
+                indent_level += 1
+            self.stream.advance()
+
+        #envia para o IndentManager e processa a indentação
+        if self.stream.peek() not in ("\r", "\n", "#", ""):
+            tokens = process_indentation(indent_level, start_line, start_col)
+            if tokens:
+                self.pending_tokens.extend(tokens) 
+
+    #envia DEDENT pedentes e retorna o token EOF
+    def _finalize_and_get_eof(self) -> Token:
+        dedent_tokens = finalize_indentation(self.stream.line, self.stream.column)
+        if dedent_tokens:
+            self.pending_tokens.extend(dedent_tokens)
+            return self.pending_tokens.popleft()
+        return Token(TokenType.EOF, "", None, self.stream.line, self.stream.column)
+
+    #esse metodo é o coração do scanner ele roda todos os outros scanners 
     def next_token(self) -> Token:
-        self._skip_whitespace_and_comments()
-        if self.stream.is_at_end():
-            return Token(TokenType.EOF, "", None, self.stream.line, self.stream.column)
+        #aqui é onde guarda  tokens de identação na fila 
+        if self.pending_tokens:
+            return self.pending_tokens.popleft()
 
+        #roda toda vez que inicializa uma linha
+        if self.at_line_start:
+            self._process_line_indentation()
+            if self.pending_tokens:
+                return self.pending_tokens.popleft() 
+             
+        if self.stream.is_at_end():
+            return self._finalize_and_get_eof()
+
+        self._skip_whitespace()
+
+        if self.stream.peek() == "#":
+            self._skip_comment()
+            return self.next_token()
+
+        if self.stream.peek() in ("\r", "\n"):
+            self._scan_newline()
+            return self.next_token()
+        
         character = self.stream.peek()
         line = self.stream.line
         column = self.stream.column
